86bf9db59200db6301481f0763a1da2b9311accc
Josh Weinstein <josh@goodcrush.com>
2012-01-24 20:21:46 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elevatorux::Application.config.secret_token = '5ba1fe6c0d611f3111d78e219b4091854b1165d0b340462a8fde01fa3b5e93ba8673147e1d8f764d1f1c2204e364ee4fe5a78cc6dc4730623b54241160585929'
