1c58cd300273a741203798e7704c6b6d40b90589
Josh Weinstein <josh@goodcrush.com>
2012-01-27 23:15:06 -0500

    fix config issue w sessions

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Elevatorux::Application.initialize!
-
-config.action_controller.session_store = :active_record_store
