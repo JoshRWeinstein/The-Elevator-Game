7327953772645c0dff9d75354cf3bb87801426a2
Josh Weinstein <josh@goodcrush.com>
2012-01-29 20:33:36 -0500

    elevatorgame

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Elevatorux::Application.config.session_store :cookie_store, :key => '_elevatorux_session'
+Elevatorgame::Application.config.session_store :cookie_store, :key => '_elevatorgame_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
