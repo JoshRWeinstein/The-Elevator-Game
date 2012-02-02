e2337836b9b0ddf704f7187116ce90900957bcfc
Josh Weinstein <josh@goodcrush.com>
2012-02-01 20:00:50 -0500

    try to run without the larger stuff

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -111,10 +111,10 @@ class RidesController < ApplicationController
   end
   
   def results
-    @Usersession = Usersession.all
-    @UsersessionCount = Usersession.count
-    @Ride = Ride.all
-    @RidesCount = Ride.count
+    #@Usersession = Usersession.all
+    #@UsersessionCount = Usersession.count
+    #@Ride = Ride.all
+    #@RidesCount = Ride.count
     @lobby0 = Ride.find(:all, :conditions => "config = 0 and floor = 1").count
     @lobby0top = Ride.find(:all, :conditions => "top = true and config = 0 ").count
     #@lobby0notnull = Ride.find(:all, :conditions => "top IS NOT NULL and config = 0 and floor = 1").count
