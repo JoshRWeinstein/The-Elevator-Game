4cb68d3392f2c9c4a85191634a82d842418b1e08
Josh Weinstein <josh@goodcrush.com>
2012-02-07 13:18:36 -0500

    new secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Elevatorgame::Application.config.secret_token = '#'
+Elevatorgame::Application.config.secret_token = "7b98a46786dce162b6aaa593fb78d566e9583d2408827ba8d4ab696c73fbd2fbef7d6ff2a4d6372a702df223ea300c341ff1e54878870093551e2080ac3669db"
